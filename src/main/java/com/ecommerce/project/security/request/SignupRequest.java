5b81e1cc5185863c9b978f6888d7b7f97631ee74
VEN <ven@Mac.tbcnet.net.tw>
2025-08-03 03:40:13 +0800

    before cloudinary

--- src/main/java/com/ecommerce/project/security/request/SignupRequest.java
+++ src/main/java/com/ecommerce/project/security/request/SignupRequest.java
@@ -4,10 +4,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 public class SignupRequest {
     @NotBlank
     @Size(min = 3, max = 20)
